ad636bf6843cc60c45405ec56560b50b5be1f34c
digi5D-git <digi5D@disroot.org>
2025-09-21 16:01:41 -0400

    Sync: 250121 16:01 (AxAttack)

---
+++ Pocket/Templates/Scripts/getCategories.js
@@ -0,0 +1,18 @@
+function getCategories() {
+    return [
+        "Programming Languages",
+        "Web Development", 
+        "Computer Science",
+        "Mathematics",
+        "Data Formats",
+        "Tools & Utilities",
+        "Frameworks & Libraries",
+        "Databases",
+        "DevOps",
+        "Security",
+        "AI/Machine Learning",
+        "Mobile Development"
+    ];
+}
+
+module.exports = getCategories;
